543f6e89c4c39e082c469a82385b1bf4deaeaf45
DAW <DAW@localhost>
2017-10-30 08:53:58 +0100

    Practica3

--- public_html/Practica3/arrays.js
+++ public_html/Practica3/arrays.js
@@ -15,6 +15,14 @@ function mostrarArray(){
     }
 }
 
+function primerUltimo(){
+    var primero = lista_numeros[0];
+    var long = (lista_numeros.length) - 1;
+    var ultimo = lista_numeros[long];
+    var info = document.getElementById("info");
+    info.innerHTML = "Primero: " + primero + " / Último: " + ultimo;
+}
+
 function addUltimo(){
     var numero = window.prompt("Numero: ");
     lista_numeros.push(numero);
@@ -39,30 +47,56 @@ function borrarPrimero(){
 
 function calcular(){
     var numero = window.prompt("*, + , / , -");
+    var suma = 0;
+    var multi = 0;
+    var div = 0;
+    var resta = 0;
     if(numero == "*"){
         for (i=0;i<lista_numeros.length;i++){
             multi = multi * lista_numeros[i]; 
         }
-        info.innerHTML =
+        var info = document.getElementById("resultado");
+        info.innerHTML = "El resultado del producto es: " + multi;
     }
     else if(numero == "+"){
         for (i=0;i<lista_numeros.length;i++){
             suma = suma + lista_numeros[i]; 
         }
-        
+        var info = document.getElementById("resultado");
+        info.innerHTML = "El resultado de la suma es: " + suma;
     }
     else if(numero == "/"){
         for (i=0;i<lista_numeros.length;i++){
             div = div / lista_numeros[i]; 
         }
-        
+        var info = document.getElementById("resultado");
+        info.innerHTML = "El resultado dela division es: " + div;
     }
     else if(numero == "-"){
         for (i=0;i<lista_numeros.length;i++){
             resta = resta - lista_numeros[i]; 
         }
-        
+        var info = document.getElementById("resultado");
+        info.innerHTML = "El resultado de la resta es: " + resta;
     }
     
+}
+
+function borrarPosicion(){
+    var numero = window.prompt("Posicion: ");
+    lista_numeros.splice(numero, 1);
+    mostrarArray();
+}
+
+function borrarNumero(){
+    var numero = window.prompt("Numero a borrar: ");
+    for(i=0;i<lista_numeros.length;){
+        if(lista_numeros[i] == numero){
+            lista_numeros.splice(i, 1);
+        }else{
+            i++;
+        }
+    }
+    mostrarArray();
 }
 
