b2e32f0eb1bb3b1f11274d171267b0ab7d1478f3
DAW <DAW@localhost>
2017-12-20 11:32:30 +0100

    practica6

---
+++ public_html/Practica6/newjavascript.js
@@ -0,0 +1,6 @@
+//Valor maximo
+document.getElementById("max").innerHTML = Number.MAX_VALUE;
+//Valor minimo
+document.getElementById("min").innerHTML = Number.MIN_VALUE;
+
+document.getElementById("altura").innerHTML = window.screenY;
