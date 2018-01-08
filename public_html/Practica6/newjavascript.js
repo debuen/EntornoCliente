d21746b115faef4cc69d5df903ebc82eff36c829
DAW <DAW@localhost>
2018-01-08 09:01:17 +0100

    practica6 proceso

--- public_html/Practica6/newjavascript.js
+++ public_html/Practica6/newjavascript.js
@@ -2,5 +2,19 @@
 document.getElementById("max").innerHTML = Number.MAX_VALUE;
 //Valor minimo
 document.getElementById("min").innerHTML = Number.MIN_VALUE;
-
-document.getElementById("altura").innerHTML = window.screenY;
+//Altura pantalla
+document.getElementById("altura").innerHTML = screen.height;
+//Anchura pantalla
+document.getElementById("ancho").innerHTML = screen.width;
+//Altura Web
+document.getElementById("webHeight").innerHTML = window.innerHeight;
+//Anchura Web
+document.getElementById("webWidth").innerHTML = window.innerWidth;
+//URL
+document.getElementById("url").innerHTML = location.href;
+//Web con extension
+document.getElementById("index").innerHTML = location.pathname;
+//Numero Random
+document.getElementById("random").innerHTML = Math.floor((Math.random() * 200) + 0);
+//Sistema Operativo
+document.getElementById("SO").innerHTML = navigator.appName;
